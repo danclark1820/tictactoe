fb57d24a940abbb9a4ae6eeedc9390f43dedce8b
danclark1820 <danclark1820@gmail.com>
2015-05-09 19:44:37 -0400

    commiting the bit of work that was done today

--- public/index.js
+++ public/index.js
@@ -1,4 +1,26 @@
 var divOne = document.getElementById('one');
-if (divOne) {
-  divOne.className += divOne.className ? ' o' : 'o';
+var divTwo = document.getElementById('two');
+var divThree = document.getElementById('three');
+var divFour = document.getElementById('four');
+var divFive = document.getElementById('five');
+var divSix = document.getElementById('six');
+var divSeven = document.getElementById('seven');
+var divEight = document.getElementById('eight');
+var divNine = document.getElementById('nine');
+
+
+var o = document.createTextNode('O');
+var x = document.createTextNode('X')
+
+function addO(d) {
+  if (d) {
+    d.className += d.className ? ' o' : 'o';
+    d.appendChild(o);
+  }
+};
+
+function abc() {
+  alert("wahoo")
 }
+
+divOne.onclick = addO(divOne);
