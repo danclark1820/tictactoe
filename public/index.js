7b413e8182c4915e652c1bf89ed5eaa781df51a0
danclark1820 <danclark1820@gmail.com>
2015-05-12 10:21:26 -0400

    add checkWin, hasClass, addX, game

--- public/index.js
+++ public/index.js
@@ -8,19 +8,44 @@ var divSeven = document.getElementById('seven');
 var divEight = document.getElementById('eight');
 var divNine = document.getElementById('nine');
 
+function addX(){
+  if (this) {
+    this.className += this.className ? ' x' : 'x'
+    this.appendChild(document.createTextNode('X'))
+  }
+}
 
-var o = document.createTextNode('O');
-var x = document.createTextNode('X')
-
-function addO(d) {
-  if (d) {
-    d.className += d.className ? ' o' : 'o';
-    d.appendChild(o);
+function addO(){
+  if (this) {
+    this.className += this.className ? ' o' : 'o'
+    this.appendChild(document.createTextNode('O'));
   }
 };
 
-function abc() {
-  alert("wahoo")
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
 }
 
-divOne.onclick = addO(divOne);
+function checkWin(elem1, elem2, elem3) {
+  if (hasClass(elem1, 'o') && hasClass(elem2, 'o') && hasClass(elem3, 'o')) {
+    alert("O Wins!");
+    return true;
+  } else if (hasClass(elem1, 'x') && hasClass(elem2, 'x') && hasClass(elem3, 'x')) {
+    alert("X Wins!");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+function game() {
+  var count = 0;
+  var turn = true
+  while (count < 9){
+    if (turn == true) {
+      
+    }
+  }
+}
